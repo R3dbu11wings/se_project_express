669b9a3a7a957b22fbf3b818ec647ea212ac388d
Zacharytesch <zachctesch_10@hotmail.com>
2025-11-18 00:26:22 -0600

    fixing error

--- app.js
+++ app.js
@@ -14,6 +14,12 @@ mongoose
 
 app.use(express.json());
 app.use("/", router);
+app.use((req, res, next) => {
+  req.user = {
+    _id: "5d8b8592978f8bd833ca8133",
+  };
+  next();
+});
 
 app.listen(PORT, () => {
   console.log(`listening on port ${PORT}`);
